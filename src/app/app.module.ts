93d1633643c151321ae6056b2948b0cfafc21834
tate_777 <estate.keldishvili.1@btu.edu.ge>
2023-11-16 01:56:36 +0400

    Refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,10 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http';
-
-import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {MyServiceService} from './my-service.service';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {NewHomeComponents} from "./AddingPage/addHome";
-import {RouterModule, Routes} from "@angular/router";
 import {AuthorizationPage} from "./authorizationPage/AuthorizationPage";
-
-const routes: Routes = [
-  {path: 'newHome', component: NewHomeComponents},
-];
 
 @NgModule({
   declarations: [
@@ -21,11 +13,9 @@ const routes: Routes = [
   imports: [
     AuthorizationPage,
     BrowserModule,
-    AppRoutingModule,
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
-    RouterModule.forRoot(routes)
   ],
   providers: [MyServiceService],
   bootstrap: [AppComponent]
